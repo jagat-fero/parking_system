f4879561acb94973a1b6bb4386f626fbddd42a33
Jagat Panchal <jagat.panchal@fero.ai>
2023-01-17 11:57:19 +0530

    Fixing a Secret Key issues on GitHub Repo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!r30dd3_l4paxfzp5uy^dbwq)*vfwt#yflo5t#0j8jv#*bafpq'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = env.bool('DEBUG', default=True)
+DOMAIN = env.str("DOMAIN")
+DOMAIN_IP = env.str("DOMAIN_IP")
+ALLOWED_HOSTS = [DOMAIN, DOMAIN_IP]
 
 
 # Application definition
